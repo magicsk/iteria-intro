46c4eda2dd92c6d6f72ce14e41d9aacc2333e8cb
magicsk <magicsk@magicsk.eu>
2021-11-12 20:45:48 +0100

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-/* eslint-disable react/prefer-stateless-function */
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  Route, Link, Routes,
+  Link,
+  Route, Routes,
 } from 'react-router-dom';
 import {
   Box,
-  Button,
   Grommet,
   ResponsiveContext,
-  Header,
-  Menu,
-  Heading,
 } from 'grommet';
 import { Home } from 'grommet-icons';
 import { ApolloProvider } from '@apollo/client';
 import client from './ApolloClient/client';
 import { ListOfCustomers, ListOfOrders } from './Lists';
+import './App.css';
+import { AppHeader } from './AppHeader';
 
 const theme = {
   global: {
     colors: {
-      brand: '#228BE6',
+      brand: '#bb454e',
     },
     font: {
       family: 'Roboto',
@@ -30,37 +28,29 @@ const theme = {
   },
 };
 
-class App extends React.Component {
-  render() {
+class App extends Component {
+  render(): JSX.Element {
     return (
       <Grommet theme={theme} full>
         <ApolloProvider client={client}>
           <ResponsiveContext.Consumer>
             {() => (
               <Box fill>
-                <Header background="brand">
-                  <Box direction="row">
-                    <Link to="/"><Button icon={<Home />} hoverIndicator /></Link>
-                  </Box>
+                <AppHeader
+                  appName="Customer's sheet"
+                  appIcon={<Link to="/"><Home /></Link>}
+                />
+                <Box flex margin={{ horizontal: 'large' }} overflow={{ horizontal: 'hidden' }} align="center" justify="start" className="main">
                   <Routes>
-                    <Route path="/" element={<Heading level="3" margin="none">List of customers</Heading>} />
-                    <Route path="orders" element={<Heading level="3" margin="none">List of orders</Heading>} />
+                    <Route path="/" element={<ListOfCustomers />} />
+                    <Route path="orders" element={<ListOfOrders />} />
                   </Routes>
-                  <Menu label="account" items={[{ label: 'logout' }]} />
-                </Header>
-                <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
-                  <Box flex align="center" justify="start">
-                    <Routes>
-                      <Route path="/" element={<ListOfCustomers />} />
-                      <Route path="orders" element={<ListOfOrders />} />
-                    </Routes>
-                  </Box>
                 </Box>
               </Box>
             )}
           </ResponsiveContext.Consumer>
         </ApolloProvider>
-      </Grommet>
+      </Grommet >
     );
   }
 }
--- src/Lists.tsx
+++ src/Lists.tsx
@@ -1,38 +1,80 @@
 import {
   Table, TableHeader, TableRow, TableCell, TableBody,
+  Spinner, Box, Avatar,
+  CardHeader, CardBody, Card, Text,
 } from 'grommet';
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useQuery, gql } from '@apollo/client';
 import './Lists.css';
+import { User } from 'grommet-icons';
 
 const CLIENT_LIST = gql`
-  query GetClientList {
-    client_list {
-      id
-      client_name
-      date_of_birth
-      vip
-      sum_of_orders
+query GetClientList {
+  client_list {
+    id
+    client_name
+    date_of_birth
+    vip
+    orders_aggregate {
+      aggregate {
+        count
+      }
     }
   }
+}
 `;
+
+interface OrdersList {
+  'client_id': number,
+  'id': number,
+  'order_date': string,
+  'price': string,
+  'sum_of_products': number
+}
+
+interface ClientsList {
+  'id': number,
+  'client_name': string,
+  'date_of_birth': string,
+  'vip': boolean,
+  'orders'?: Array<OrdersList>,
+  'orders_aggregate': {
+    'aggregate': {
+      'count': number,
+      'sum'?: {
+        'price': string,
+        'sum_of_products': number
+      }
+    }
+  }
+}
+
+interface CustomersList {
+  'client_list': Array<ClientsList>
+}
+
+const vipEl = (con: boolean) =>{
+  if (con) {
+    return <Text style={{ fontWeight: 'bold' }}>VIP</Text>;
+  }
+};
+
 
 function getFormattedDate(stringDate: string): string {
   const date = new Date(stringDate);
   return date.toLocaleDateString().toString();
 }
 
-function ListOfCustomers(): React.ReactElement<{}> {
+function ListOfCustomers(): React.ReactElement {
   const navigate = useNavigate();
-  const { data, loading, error } = useQuery(CLIENT_LIST);
+  const { data, loading, error } = useQuery<CustomersList>(CLIENT_LIST);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <Spinner />;
   }
 
   if (error) {
-    return <div>{error}</div>;
+    return <Text>{error}</Text>;
   }
 
   return (
@@ -40,16 +82,16 @@ function ListOfCustomers(): React.ReactElement<{}> {
       <TableHeader>
         <TableRow>
           <TableCell scope="col" border="bottom">
-            Id
+            Client ID
           </TableCell>
           <TableCell scope="col" border="bottom">
-            Name
+            Customer
           </TableCell>
           <TableCell scope="col" border="bottom">
             Date of birth
           </TableCell>
           <TableCell scope="col" border="bottom">
-            Vip status
+            Vip
           </TableCell>
           <TableCell scope="col" border="bottom">
             Sum of orders
@@ -58,10 +100,11 @@ function ListOfCustomers(): React.ReactElement<{}> {
       </TableHeader>
       <TableBody>
         {data.client_list.map(({
-          id, client_name, date_of_birth, vip, sum_of_orders,
+          id, client_name, date_of_birth, vip, orders_aggregate,
         }) => (
           <TableRow
             key={id}
+            style={{ cursor: 'pointer' }}
             className="mainTableRow"
             onClick={() => {
               navigate(`/orders?id=${id}`);
@@ -73,9 +116,9 @@ function ListOfCustomers(): React.ReactElement<{}> {
             <TableCell scope="row">
               <strong>{client_name}</strong>
             </TableCell>
-            <TableCell>{date_of_birth}</TableCell>
-            <TableCell>{vip ? 'valid' : 'invalid'}</TableCell>
-            <TableCell>{sum_of_orders}</TableCell>
+            <TableCell>{getFormattedDate(date_of_birth)}</TableCell>
+            <TableCell>{vip ? '✅' : '❌'}</TableCell>
+            <TableCell>{orders_aggregate.aggregate.count}</TableCell>
           </TableRow>
         ))}
       </TableBody>
@@ -83,67 +126,113 @@ function ListOfCustomers(): React.ReactElement<{}> {
   );
 }
 
-function ListOfOrders(): React.ReactElement<{}> {
+function ListOfOrders(): React.ReactElement {
   const paramValue = new URLSearchParams(window.location.search).get('id');
   const ORDER_LIST = gql`
   query GetOrderList {
-    orders_list(where: {client_id: {_eq: ${paramValue}}}) {
+    client_list(where: {id: {_eq: ${paramValue}}}) {
+      client_name
+      date_of_birth
       id
-      order_date
-      price
-      sum_of_products
+      vip
+      orders {
+        client_id
+        id
+        order_date
+        price
+        sum_of_products
+      }
+      orders_aggregate {
+        aggregate {
+          count
+          sum {
+            price
+            sum_of_products
+          }
+        }
+      }
     }
   }
 `;
 
-  const { data, loading, error } = useQuery(ORDER_LIST);
+  const { data, loading, error } = useQuery<CustomersList>(ORDER_LIST);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <Spinner />;
   }
 
   if (error) {
     return <div>{error}</div>;
   }
 
+  const client = data.client_list[0];
+  const clientDyn = data.client_list[0].orders_aggregate.aggregate;
   return (
-    <Table className="mainTable" alignSelf="stretch">
-      <TableHeader>
-        <TableRow>
-          <TableCell scope="col" border="bottom">
-            Id
-          </TableCell>
-          <TableCell scope="col" border="bottom">
-            Date of order
-          </TableCell>
-          <TableCell scope="col" border="bottom">
-            Price
-          </TableCell>
-          <TableCell scope="col" border="bottom">
-            Sum of products
-          </TableCell>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data.orders_list.map(({
-          id, order_date, price, sum_of_products,
-        }) => (
-          <TableRow
-            key={id}
-            className="mainTableRow"
-          >
-            <TableCell scope="row">
-              {id}
+    <Box gap="medium" alignSelf="stretch" justify="between">
+      <Card direction="row" margin="small" background="light-1">
+        <CardHeader pad="small" justify="start">
+          <Avatar background="accent-4">
+            <User />
+          </Avatar>
+          <Text alignSelf="center">{client.client_name}</Text>{vipEl(client.vip)}
+        </CardHeader>
+        <CardBody pad="small" direction="row">
+          <Text alignSelf="center" margin="small">
+            {`Id: ${client.id}`}
+          </Text>
+          <Text alignSelf="center" margin="small">
+            {`Date of birth: ${getFormattedDate(client.date_of_birth)}`}
+          </Text>
+          <Text alignSelf="center" margin="small">
+            {`Orders: ${clientDyn.count}`}
+          </Text>
+          <Text alignSelf="center" margin="small">
+            {`Products: ${clientDyn.sum.sum_of_products}`}
+          </Text>
+          <Text alignSelf="center" margin="small">
+            {`Lifetime spendings: ${clientDyn.sum.price}`}
+          </Text>
+
+        </CardBody>
+      </Card>
+      <Table className="mainTable" alignSelf="stretch">
+        <TableHeader>
+          <TableRow>
+            <TableCell scope="col" border="bottom">
+              ID
             </TableCell>
-            <TableCell scope="row">
-              <strong>{getFormattedDate(order_date)}</strong>
+            <TableCell scope="col" border="bottom">
+              Date of order
             </TableCell>
-            <TableCell>{price}</TableCell>
-            <TableCell>{sum_of_products}</TableCell>
+            <TableCell scope="col" border="bottom">
+              Price
+            </TableCell>
+            <TableCell scope="col" border="bottom">
+              Products
+            </TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {data.client_list[0].orders.map(({
+            id, order_date, price, sum_of_products,
+          }) => (
+            <TableRow
+              key={id}
+              className="mainTableRow"
+            >
+              <TableCell scope="row">
+                {id}
+              </TableCell>
+              <TableCell scope="row">
+                <strong>{getFormattedDate(order_date)}</strong>
+              </TableCell>
+              <TableCell>{price}</TableCell>
+              <TableCell>{sum_of_products}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Box>
   );
 }
 
